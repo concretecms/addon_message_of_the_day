c45a4a16a852133ad412cb2da0ecb352c66da2d6
Ryan Tyler <ryan@concrete5.org>
2012-01-18 17:28:43 -0800

    made sample content go into stacks, updated version info

--- packages/message_of_the_day/controller.php
+++ packages/message_of_the_day/controller.php
@@ -5,8 +5,8 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 class MessageOfTheDayPackage extends Package {
 
 	protected $pkgHandle = 'message_of_the_day';
-	protected $appVersionRequired = '5.3.2';
-	protected $pkgVersion = '1.0';  
+	protected $appVersionRequired = '5.5';
+	protected $pkgVersion = '1.1';  
 	
 	public function getPackageName() {
 		return t("Message of the day"); 
@@ -27,23 +27,23 @@ class MessageOfTheDayPackage extends Package {
 	private function addScrapbookContent($pkg) {	
 		try {
 			$xml = simplexml_load_file($pkg->getPackagePath() .'/seed_messages.xml','SimpleXMLElement', LIBXML_NOCDATA);
-			
 			$u = new User();
-			$scrapbookPage= Page::getByPath('/dashboard/scrapbook');
+			
+			Loader::model('stack');
 			
 			foreach($xml->scrapbook as $sb) {
 				$title = t('Message of the day - ').$sb->title;
-				$a = Area::get($scrapbookPage, $title);
-				if (!is_object($a)) {
-					$a = Area::getOrCreate( $scrapbookPage, $title);
-				}
+				
+				Stack::addStack($title);
+				$stack = Stack::getByName($title);
 				
 				foreach($sb->content as $content) {
 					$bt = BlockType::getByHandle('content');
 					$data = array();
 					$data['uID'] = $u->getUserID();
 					$data['content'] = $content;
-					$scrapbookPage->addBlock($bt, $title, $data);
+					$stackArea = Area::get($stack,STACKS_AREA_NAME);
+					$stack->addBlock($bt, $stackArea, $data);
 				}
 			}
 		} catch ( Exception $e ) {
